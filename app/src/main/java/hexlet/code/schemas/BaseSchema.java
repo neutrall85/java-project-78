6b2c94fbaeae84da55eb0dd52abbba99bd01e21d
neutrall85 <canni85@mail.ru>
2025-09-20 19:13:48 +0400

    fix linter

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -7,7 +7,7 @@ import java.util.function.Predicate;
 public abstract class BaseSchema<T> {
     private final Map<String, Predicate<T>> checks = new HashMap<>();
 
-    public void addCheck(String name, Predicate<T> check) {
+    public final void addCheck(String name, Predicate<T> check) {
         this.checks.put(name, check);
     }
 
